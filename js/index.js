e7c5cdeab405b41057702f5eb2f1865d2fdd88b3
Rereloluwa Olubiyi <rereloluwaolubiyi@Rereloluwas-Air.lan>
2026-03-04 02:48:32 -0600

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -33,18 +33,40 @@ const skills = [
 
   const skillsList = skillsSection.querySelector("ul");
 
-  for (let i = 0; i < skills.length; i++) {
-
-    const skill = document.createElement("li");
-  }
-
-  for (let i = 0; i < skills.length; i++) {
-    const skill = document.createElement("li");
-    skill.innerText = skills[i];
-  }
 
   for (let i = 0; i < skills.length; i++) {
     const skill = document.createElement("li");
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
   }
+
+  const messageForm = document.forms["leave_message"];
+
+  messageForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+
+    const userName = event.target.usersName.value;
+    const userEmail = event.target.usersEmail.value;
+    const userMessage = event.target.usersMessage.value;
+
+    console.log(userName, userEmail, userMessage);
+
+    const messageSection = document.querySelector("#messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+
+    newMessage.innerHTML = `<a href="mailto:${userEmail}">${userName}</a> <span>${userMessage}</span>`;
+
+    const removeButton = document.createElement("button");
+
+removeButton.innerText = "remove";
+removeButton.type = "button";
+removeButton.addEventListener("click", function(event) {
+  const entry = event.target.parentNode;
+  entry.remove();
+});
+newMessage.appendChild(removeButton);
+messageList.appendChild(newMessage);
+event.target.reset();
+});
