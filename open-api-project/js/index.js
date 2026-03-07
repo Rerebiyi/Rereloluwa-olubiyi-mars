30b6e3c402d218137186231a9ee8163de96dc41f
Rereloluwa Olubiyi <rereloluwaolubiyi@Rereloluwas-Air.lan>
2026-03-07 01:46:47 -0600

    Lesson 14: added second fetch, updated navigation, started deeper content display, and basic styling

--- open-api-project/js/index.js
+++ open-api-project/js/index.js
@@ -1,21 +1,69 @@
-fetch("https://www.swapi.tech/api/people")
-  .then(response => {
+document.addEventListener("DOMContentLoaded", function () {
+  const characterButton = document.querySelector("#show-characters");
+  const planetButton = document.querySelector("#show-planets");
+  const loading = document.querySelector("#loading");
+  const list = document.querySelector("#character-list");
+  const sectionTitle = document.querySelector("#section-title");
+
+  function fetchData(url, type) {
+    loading.style.display = "block";
+    list.innerHTML = "";
+
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const results = data.results;
+        let loadedItems = 0;
+
+        results.forEach(function (item) {
+          fetch(item.url)
+  .then(function (response) {
     return response.json();
   })
-  .then(data => {
-    const characters = data.results;
+  .then(function (detailData) {
+    const properties = detailData.result.properties;
 
-    const loading = document.querySelector("#loading");
-    loading.style.display = "none";
-  
-    const characterList = document.querySelector("#character-list");
-  
-    for (let i = 0; i < characters.length; i++) {
-      const listItem = document.createElement("li");
-      listItem.innerText = `${characters[i].name} (ID: ${characters[i].uid})`;
-      characterList.appendChild(listItem);
+    const listItem = document.createElement("li");
+
+    if (type === "characters") {
+      listItem.innerText =
+        properties.name +
+        " | Height: " + properties.height +
+        " | Gender: " + properties.gender;
     }
-  })
-  .catch(error => {
-    console.error("Error:", error);
+    
+    if (type === "planets") {
+      listItem.innerText =
+        properties.name +
+        " | Climate: " + properties.climate +
+        " | Population: " + properties.population;
+    }
+
+    list.appendChild(listItem);
+
+    loadedItems++;
+
+if (loadedItems === results.length) {
+  loading.style.display = "none";
+}
   });
+      
+        });
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  }
+
+  characterButton.addEventListener("click", function () {
+    sectionTitle.innerText = "Character List";
+    fetchData("https://www.swapi.tech/api/people", "characters");
+  });
+
+  planetButton.addEventListener("click", function () {
+    sectionTitle.innerText = "Planet List";
+    fetchData("https://www.swapi.tech/api/planets", "planets");
+  });
+});
