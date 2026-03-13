3c7aef46895f730eec898e670bb3e9e739d33d47
Rereloluwa Olubiyi <rereloluwaolubiyi@Rereloluwas-Air.lan>
2026-03-12 23:41:57 -0500

    Fix Portfolio CSS issues and API loading message

--- open-api-project/README.md
+++ open-api-project/README.md
@@ -1,1 +1,25 @@
-# open-api-project
+# Star Wars API Project
+
+This project displays information from the Star Wars API (SWAPI).
+
+Users can click buttons to view:
+- Star Wars characters
+- Star Wars planets
+
+Each button makes a separate API request to retrieve the data.
+
+## How to Run the Project
+
+1. Download or clone this repository.
+2. Open the project folder.
+3. Open the file **index.html** in your web browser.
+
+Optional:  
+If you are using VS Code, you can right-click **index.html** and select **Open with Live Server**.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- SWAPI (Star Wars API)
--- open-api-project/js/index.js
+++ open-api-project/js/index.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function fetchData(url, type) {
     loading.style.display = "block";
+    loading.innerText = "Loading " + type + "...";
     list.innerHTML = "";
 
     fetch(url)
@@ -19,37 +20,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
         results.forEach(function (item) {
           fetch(item.url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (detailData) {
-    const properties = detailData.result.properties;
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (detailData) {
+              const properties = detailData.result.properties;
 
-    const listItem = document.createElement("li");
+              const listItem = document.createElement("li");
 
-    if (type === "characters") {
-      listItem.innerText =
-        properties.name +
-        " | Height: " + properties.height +
-        " | Gender: " + properties.gender;
-    }
-    
-    if (type === "planets") {
-      listItem.innerText =
-        properties.name +
-        " | Climate: " + properties.climate +
-        " | Population: " + properties.population;
-    }
+              if (type === "characters") {
+                listItem.innerText =
+                  properties.name +
+                  " | Height: " + properties.height +
+                  " | Gender: " + properties.gender;
+              }
 
-    list.appendChild(listItem);
+              if (type === "planets") {
+                listItem.innerText =
+                  properties.name +
+                  " | Climate: " + properties.climate +
+                  " | Population: " + properties.population;
+              }
 
-    loadedItems++;
+              list.appendChild(listItem);
 
-if (loadedItems === results.length) {
-  loading.style.display = "none";
-}
-  });
-      
+              loadedItems++;
+
+              if (loadedItems === results.length) {
+                loading.style.display = "none";
+              }
+            });
         });
       })
       .catch(function (error) {
